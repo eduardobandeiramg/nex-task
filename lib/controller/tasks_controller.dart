0b7b2de63c08712cda5bd5425ee5188d937a4936
eduardobandeiramg <eduardobandeiramg@gmail.com>
2025-07-27 21:14:43 -0300

    creating fetching tasks data functions

--- lib/controller/tasks_controller.dart
+++ lib/controller/tasks_controller.dart
@@ -1,10 +1,14 @@
 import 'package:nex_task/model/models/task.dart';
 import 'package:nex_task/services/database/tasks_database.dart';
 
-class TasksController{
-
+class TasksController {
   static createTask(Task task) async {
     var response = await TasksDatabase.createTask(task);
     print("RESPONSE: $response");
+  }
+
+  static getTasks() async {
+    var response = await TasksDatabase.getTasks();
+    print("RESPOSTA DO GET TASKS PARA O CONTROLLER: $response");
   }
 }
--- lib/services/database/tasks_database.dart
+++ lib/services/database/tasks_database.dart
@@ -1,10 +1,21 @@
+import 'package:nex_task/view/state_management/user_infos/user_infos.dart';
+
 import '../../model/models/task.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class TasksDatabase {
   static Future<dynamic> createTask(Task task) async {
     var response = await Supabase.instance.client.from("tasks").insert(task.toMap());
-    print("RESPOSTA: $response");
+    print("RESPOSTA DO CREATE: $response");
+    return response;
+  }
+
+  static Future<dynamic> getTasks() async {
+    var response = await Supabase.instance.client
+        .from("tasks")
+        .select()
+        .eq("id_user", UserInfos.userID);
+    print("RESPOSTA DO GET TASKS: $response");
     return response;
   }
 }
